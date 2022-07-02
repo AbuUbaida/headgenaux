38db049e559afc6b5ff4a0dfae0f62569f85f414
Abu Ubaida Akash <abu.ubaida1206@gmail.com>
2022-07-02 23:44:34 +0600

    testing tpu

--- test_run.py
+++ test_run.py
@@ -1,118 +1,98 @@
+import os
 import datasets
 import transformers
-import evaluate
 import pandas as pd
 import numpy as np
 import torch
-import time
+import torch_xla
 import torch_xla.core.xla_model as xm
-from tqdm.notebook import tqdm
-from transformers import BertTokenizerFast, AutoTokenizer, Seq2SeqTrainingArguments, Seq2SeqTrainer, EncoderDecoderModel
-from normalizer import normalize
-from datasets import load_dataset, DatasetDict, Dataset, ClassLabel
-from bangla_rouge_score import rouge_scorer, scoring
+from transformers import (BertTokenizerFast, AutoTokenizer, Seq2SeqTrainingArguments, Seq2SeqTrainer, EncoderDecoderModel,
+                            get_linear_schedule_with_warmup)
+from datasets import load_dataset, DatasetDict, Dataset
 from torch.utils.data import DataLoader
 
-tokenizer = AutoTokenizer.from_pretrained("csebuetnlp/banglabert")
-train_data = load_dataset('csv', data_files='data/palokal_merged_with_cap_v1.0.csv', split='train')
-# val_data = load_dataset('csv', data_files='data/valid_data_captioned.csv', split='train')
+tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
 
-batch_size = 16
+dataset = load_dataset('amazon_reviews_multi', 'en', split='train')
+dataset = dataset.train_test_split(test_size=0.1)
+val_data = dataset['test'].train_test_split(test_size=0.5)
+dataset = DatasetDict({
+    'train': dataset['train'],
+    'valid': val_data['train'],
+    'test': val_data['test'],})
+train_data = dataset['train']
+
+batch_size = 4
 encoder_max_length = 512
-decoder_max_length = 32
+decoder_max_length = 128
 
 def process_data_to_model_inputs(batch):
-    for idx, example in enumerate(batch['head_lines']):
-        batch['head_lines'][idx] = normalize(example)
-    for idx, example in enumerate(batch['article']):
-        batch['article'][idx] = normalize(example)
-        
-    # tokenize the inputs and labels 
-    inputs = tokenizer(batch["article"], padding="max_length", truncation=True, max_length=encoder_max_length)
-    outputs = tokenizer(batch["head_lines"], padding="max_length", truncation=True, max_length=decoder_max_length)
+    inputs = tokenizer(batch['review_body'], padding="max_length", truncation=True, max_length=encoder_max_length)
+    outputs = tokenizer(batch['review_title'], padding="max_length", truncation=True, max_length=decoder_max_length)
 
     batch["input_ids"] = inputs.input_ids
     batch["attention_mask"] = inputs.attention_mask
     batch["labels"] = outputs.input_ids.copy()
 
-    # because BERT automatically shifts the labels, the labels correspond exactly to `decoder_input_ids`. 
-    # We have to make sure that the PAD token is ignored
     batch["labels"] = [[-100 if token == tokenizer.pad_token_id else token for token in labels] for labels in batch["labels"]]
 
     return batch
 
-train_data = train_data.select(range(64))
+train_data = train_data.select(range(32))
 
 train_data = train_data.map(
     process_data_to_model_inputs, 
     batched=True, 
     batch_size=batch_size, 
-    remove_columns=["article", "head_lines", "news_link", "category"]
+    remove_columns=['language', 'product_category','review_body','review_title', 'product_id', 'review_id', 'reviewer_id', 'stars']
 )
 train_data.set_format(
-    type="torch", columns=["input_ids", "attention_mask", "labels"]
+    type="torch", columns=["input_ids", "attention_mask", "labels"],
 )
 
-# val_data = val_data.select(range(8))
+train_dataloader = DataLoader(train_data, shuffle=True, batch_size=4)
 
-# val_data = val_data.map(
-#     process_data_to_model_inputs, 
-#     batched=True, 
-#     batch_size=batch_size, 
-#     remove_columns=["article", "headline", "ic1"]
-# )
-# val_data.set_format(
-#     type="torch", columns=["input_ids", "attention_mask", "labels"],
-# )
-
-train_dataloader = DataLoader(train_data, shuffle=True, batch_size=16)
-# val_dataloader = DataLoader(val_data, batch_size=4)
-
-model = EncoderDecoderModel.from_encoder_decoder_pretrained("csebuetnlp/banglabert", "csebuetnlp/banglabert")
+model = EncoderDecoderModel.from_encoder_decoder_pretrained("bert-base-uncased", "bert-base-uncased")
 model.config.decoder_start_token_id = tokenizer.cls_token_id
 model.config.eos_token_id = tokenizer.sep_token_id
 model.config.pad_token_id = tokenizer.pad_token_id
 model.config.vocab_size = model.config.encoder.vocab_size
-model.config.max_length = 16
-model.config.min_length = 4
-model.config.no_repeat_ngram_size = 2
+model.config.max_length = 142
+model.config.min_length = 56
+model.config.no_repeat_ngram_size = 3
 model.config.early_stopping = True
-model.config.length_penalty = 1
+model.config.length_penalty = 2.0
 model.config.num_beams = 4
 
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+num_training_steps = len(train_dataloader) * 40
+
 device = xm.xla_device()
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
+optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
+scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=num_training_steps)
 print('Device: ', device)
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
 
-for epoch in range(30):  # loop over the dataset multiple times
+
+for epoch in range(30):
     print(f"Epoch:", epoch)
-    # train + evaluate on training data
-    running_loss = 0.0
-    start = time.time()
+
     for batch in train_dataloader:
         model.train()
-        # get the inputs; 
+
         input_ids = batch["input_ids"].to(device)
         attention_mask = batch["attention_mask"].to(device)
         labels = batch["labels"].to(device)
 
-        # zero the parameter gradients
         optimizer.zero_grad()
 
-        # forward + backward + optimize
         outputs = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
         loss = outputs.loss
-        # running_loss += loss.item()
+        print("Loss:", loss.item())
         loss.backward()
         # optimizer.step()
         xm.optimizer_step(optimizer, barrier=True)
-        # xm.reduce_gradients(optimizer)
-        # # Parameter Update
         # optimizer.step()
-
-        print("Running Loss:", loss.item())
-    end = time.time()
-    print('Elapsed time per epoch: ', end-start)
+        # xm.mark_step()
+        scheduler.step()
