7870cffa14d53f4b6abf06ba38abadea4b452cdd
Abu Ubaida Akash <abu.ubaida1206@gmail.com>
2022-07-03 01:18:04 +0600

    testing tpu

--- test_run.py
+++ test_run.py
@@ -6,10 +6,16 @@ import numpy as np
 import torch
 import torch_xla
 import torch_xla.core.xla_model as xm
+import torch_xla.distributed.xla_multiprocessing as xmp
+import torch_xla.distributed.parallel_loader as pl
+import warnings
 from transformers import (BertTokenizerFast, AutoTokenizer, Seq2SeqTrainingArguments, Seq2SeqTrainer, EncoderDecoderModel,
                             get_linear_schedule_with_warmup)
 from datasets import load_dataset, DatasetDict, Dataset
 from torch.utils.data import DataLoader
+from scipy import stats
+warnings.filterwarnings('ignore')
+xmp.spawn(nprocs=8)
 
 tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
 
@@ -49,8 +55,13 @@ train_data = train_data.map(
 train_data.set_format(
     type="torch", columns=["input_ids", "attention_mask", "labels"],
 )
-
-train_dataloader = DataLoader(train_data, shuffle=True, batch_size=4)
+train_sampler = torch.utils.data.DistributedSampler(
+    train_data,
+    num_replicas=xm.xrt_world_size(),
+    rank=xm.get_ordinal(),
+    shuffle=True
+)
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=4)
 
 model = EncoderDecoderModel.from_encoder_decoder_pretrained("bert-base-uncased", "bert-base-uncased")
 model.config.decoder_start_token_id = tokenizer.cls_token_id
@@ -64,21 +75,20 @@ model.config.early_stopping = True
 model.config.length_penalty = 2.0
 model.config.num_beams = 4
 
-num_training_steps = len(train_dataloader) * 40
 
 device = xm.xla_device()
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+lr = 5e-5*xm.xrt_world_size()
+num_training_steps = int(len(train_dataloader) / xm.xrt_world_size() * 30)
 model.to(device)
-optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
+optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
 scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=num_training_steps)
 print('Device: ', device)
 
-
-
 for epoch in range(30):
     print(f"Epoch:", epoch)
-
-    for batch in train_dataloader:
+    para_loader = pl.ParallelLoader(train_dataloader, [device])
+    for batch in para_loader.per_device_loader(device):
         model.train()
 
         input_ids = batch["input_ids"].to(device)
@@ -92,7 +102,7 @@ for epoch in range(30):
         print("Loss:", loss.item())
         loss.backward()
         # optimizer.step()
-        xm.optimizer_step(optimizer, barrier=True)
+        xm.optimizer_step(optimizer)
         # optimizer.step()
         # xm.mark_step()
         scheduler.step()
